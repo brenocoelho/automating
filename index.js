86cb3bab244f4038e1e4a167acf8e1e9e52ee156
Breno Coelho <breno.coelho@trusoll.com>
2017-11-19 16:32:20 -0200

    test 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-var time = require('time');
+//var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    console.log("testing pipeline");
-    currentTime.setTimezone("America/Los_Angeles");
+    //var currentTime = new time.Date(); 
+    console.log("testing pipelinels");
+    //currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Los Angeles is: Breno',// + currentTime.toString(),
     });
 };
